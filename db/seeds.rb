242b0696054e8383fb976803ded5170140f845e4
tfunk2 <tyler.j.funk2@gmail.com>
2020-05-12 05:25:32 -0600

    enabled CORS

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
-
+Puppy.destroy_all
 
 Puppy.create(
     name: "Bronx",
@@ -35,7 +35,7 @@ Puppy.create(
     name: "Charlie",
     breed: "Lab",
     description: "With a face like Charlie's, and the personality to match, it's no wonder the Lab has been the most popular breed for 26 years.",
-    imageURL: "http://cdn.akc.org/content/article-body-image/chocolate_lab_cute_puppies.jpg"
+    imageURL: "http://cdn.akc.org/content/article-body-image/black_lab_cute_puppies.jpg"
 )
 
 Puppy.create(
